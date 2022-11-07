42e386d391af6dd9481f4ea81a98708c14eebbbf
Tanya Fomina <fomina.tatianaaa@yandex.ru>
2022-11-07 21:33:22 +0200

    Update icons

--- src/index.js
+++ src/index.js
@@ -3,8 +3,7 @@
  */
 import './index.css';
 
-import IconAlignmentLeft from '../assets/alignment_left.svg';
-import IconAlignmentCenter from '../assets/alignment_center.svg';
+import { IconAlignLeft, IconAlignCenter, IconQuote } from '@codexteam/icons';
 
 /**
  * @class Quote
@@ -50,7 +49,7 @@ export default class Quote {
    */
   static get toolbox() {
     return {
-      icon: '<svg width="15" height="14" viewBox="0 0 15 14" xmlns="http://www.w3.org/2000/svg"><path d="M13.53 6.185l.027.025a1.109 1.109 0 0 1 0 1.568l-5.644 5.644a1.109 1.109 0 1 1-1.569-1.568l4.838-4.837L6.396 2.23A1.125 1.125 0 1 1 7.986.64l5.52 5.518.025.027zm-5.815 0l.026.025a1.109 1.109 0 0 1 0 1.568l-5.644 5.644a1.109 1.109 0 1 1-1.568-1.568l4.837-4.837L.58 2.23A1.125 1.125 0 0 1 2.171.64L7.69 6.158l.025.027z" /></svg>',
+      icon: IconQuote,
       title: 'Quote',
     };
   }
@@ -163,11 +162,11 @@ export default class Quote {
     return [
       {
         name: 'left',
-        icon: IconAlignmentLeft,
+        icon: IconAlignLeft,
       },
       {
         name: 'center',
-        icon: IconAlignmentCenter,
+        icon: IconAlignCenter,
       },
     ];
   }
